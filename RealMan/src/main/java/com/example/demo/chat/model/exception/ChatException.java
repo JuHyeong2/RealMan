4e10fb51aa925066ad5b0c42e7e1e0b34ff24ac8
JuHyeong2 <100174932+JuHyeong2@users.noreply.github.com>
2025-02-11 15:49:04 +0900

    Delete RealMan directory

--- RealMan/src/main/java/com/example/demo/chat/model/exception/ChatException.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.chat.model.exception;
-
-public class ChatException {
-
-}
--- RealMan/src/main/java/com/example/demo/member/model/exception/MemberException.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.member.model.exception;
-
-public class MemberException {
-
-}
