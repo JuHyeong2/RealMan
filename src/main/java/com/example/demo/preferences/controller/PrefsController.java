496efc2ed487c5e15b963489671daf84129e2a4a
성종 <ashes0921@naver.com>
2025-02-14 17:34:52 +0900

    리졸버 , 컨트롤러 맵핑

--- src/main/java/com/example/demo/preferences/controller/PrefsController.java
+++ src/main/java/com/example/demo/preferences/controller/PrefsController.java
@@ -1,6 +1,5 @@
 package com.example.demo.preferences.controller;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
