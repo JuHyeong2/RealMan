fe07172530c3cb32e2faf64b4c65a9bb3ebb54e5
desertdevv <desertdevv@gmail.com>
2025-03-04 15:04:06 +0900

    로그인로직 수정 / main으로 받아오기

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,19 +1,36 @@
 package com.example.demo;
 
+import com.example.demo.server.model.service.ServerService;
+import com.example.demo.server.model.vo.Server;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+	private final ServerService sService;
+
+
 	@GetMapping("home")
 	public String home() {
 		return "index";
 	}
 	
 	@GetMapping("/main")
-	public String mainPage() {
-		System.out.println("✅ main.html 페이지 요청됨!");
-		return "/main/main";
+	public String mainPage(Model model) {
+//  System.out.println("✅ main.html 페이지 요청됨!");
+
+	Server server = new Server();
+	ArrayList<Server> selectServerList = sService.selectServerList();
+
+	model.addAttribute("server", server);
+	model.addAttribute("selectServerList",selectServerList);
+
+	return "/main/main";
 	}
 	
 }
