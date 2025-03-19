f35e18a436cec7bf48973c8049a5aa0e76d9d15a
성종 <ashes0921@naver.com>
2025-03-19 10:54:39 +0900

    맵퍼 파라미터 두개 보내기 -> Device 객체에 담아서 하나로 보내기 (수정)

--- src/main/java/com/example/demo/preferences/model/mapper/PrefsMapper.java
+++ src/main/java/com/example/demo/preferences/model/mapper/PrefsMapper.java
@@ -2,10 +2,6 @@ package com.example.demo.preferences.model.mapper;
 
 import com.example.demo.preferences.model.vo.Device;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-
-import java.util.Map;
-
 
 @Mapper
 public interface PrefsMapper {
